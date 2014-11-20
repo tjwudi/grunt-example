e10ac9fff01b62c6a11123aab57f2787352675b8
John Wu <webmaster@leapoahead.com>
2014-11-20 16:17:23 +0800

    Step 4: Use grunt-contrib-cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,20 @@ module.exports = function(grunt) {
         files: ['www/**/*.*'],
         options: {
           livereload: true
-        },
-      },
+        }
+      }
     },
+    cssmin: {
+      min: {
+        files: {
+          'www/stylesheets/main.min.css': ['www/stylesheets/main.css']
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('build', ['cssmin']);
 };
