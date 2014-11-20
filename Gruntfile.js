4505362fdba01ebc82356b542983a7c89f2a35cd
John Wu <webmaster@leapoahead.com>
2014-11-20 16:23:14 +0800

    Step 5: Use grunt-dev-prod-switch

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,32 @@ module.exports = function(grunt) {
     cssmin: {
       min: {
         files: {
-          'www/stylesheets/main.min.css': ['www/stylesheets/main.css']
+          'dist/stylesheets/main.min.css': ['www/stylesheets/main.css']
         }
       }
+    },
+    dev_prod_switch: {
+        options: {
+            // Can be ran as `grunt --env=dev` or ``grunt --env=prod``
+            environment: grunt.option('env') || 'dev', // 'prod' or 'dev'
+            env_char: '#',
+            env_block_dev: 'env:dev',
+            env_block_prod: 'env:prod'
+        },
+        dynamic_mappings: {
+            files: [{
+                expand: true,
+                cwd: './www',
+                src: ['*.html'],
+                dest: './dist'
+            }]
+        }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-dev-prod-switch');
 
-  grunt.registerTask('build', ['cssmin']);
+  grunt.registerTask('build', ['dev_prod_switch', 'cssmin']);
 };
